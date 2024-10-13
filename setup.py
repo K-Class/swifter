49743189c3f04af7749aafdea483a8922d79f2cf
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2024-10-13 12:16:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,10 @@ setup(
     author_email="jcarpenter@manifold.ai",
     url="https://github.com/jmcarpenter2/swifter",  # use the URL to the github repo
     download_url="https://github.com/jmcarpenter2/swifter/archive/1.4.0.tar.gz",
-    keywords=["pandas", "dask", "apply", "function", "parallelize", "vectorize"],
+    keywords=["pandas", "apply", "function", "parallelize", "vectorize"],
     install_requires=[
         "pandas>=1.0.0",
         "psutil>=5.6.6",
-        "dask[dataframe]>=2.10.0",
         "tqdm>=4.33.0",
     ],
     extras_require={
